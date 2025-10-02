5649d94cb4ea5d2a13d6f7fd2de774ef2b0f42e7
galinayakibchuk <galina.yakibchuk@gmail.com>
2025-10-02 18:13:51 +0200

    added section assortment in product page

---
+++ app/js/main.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.assortment__slider', {
+  slidesPerView: 3,
+  spaceBetween: 40,
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.arrow-next',
+    prevEl: '.arrow-prev',
+  },
+
+
+});
