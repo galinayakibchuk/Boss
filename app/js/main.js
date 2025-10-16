cae2c612b9b7add7a0d7d89c47971fb8d425dd06
galinayakibchuk <galina.yakibchuk@gmail.com>
2025-10-16 17:35:56 +0200

    added reviews page

--- app/js/main.js
+++ app/js/main.js
@@ -7,6 +7,21 @@ const swiper = new Swiper('.assortment__slider', {
     nextEl: '.arrow-next',
     prevEl: '.arrow-prev',
   },
+});
 
 
+
+const swiperReviews = new Swiper('.reviews-slider', {
+  slidesPerView: 12,
+  spaceBetween: 16,
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__arrow-next',
+    prevEl: '.reviews-slider__arrow-prev',
+  },
+  pagination: {
+    el: ".reviews-slider__pagination",
+    type: "fraction",
+  },
 });
